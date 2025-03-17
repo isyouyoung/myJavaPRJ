3411ac977bb725d6f53816d393cf2beb5c0785ae
isyouyoung <2520110191@gspace.kopo.ac.kr>
2025-03-17 16:55:44 +0900

    2025-03-17 월 (숙제 2번)

---
+++ src/main/java/org/example/HW2503172.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class HW2503172 {
+    public static void main(String[] args){
+
+        int A = 3;
+        int B = 5;
+        int C = A+B;
+
+        System.out.println(C);
+    }
+}
