1fca7ab99a7781e8080f85a37b8a32b400943e0e
isyouyoung <2520110191@gspace.kopo.ac.kr>
2025-03-17 17:34:05 +0900

    첫번째 파일

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,25 +7,5 @@ public class Hello {
         System.out.println("Edit Java Code!!");
 
     }
-
-    public static class HW2503171 {
-        public static void main(String[] args){
-
-            System.out.println("Hello world!");
-        }
-    }
-
-    public static class HW2503172 {
-        public static void main(String[] args){
-
-            int A = 3;
-            int B = 5;
-            int C = A+B;
-
-            System.out.println(C);
-        }
-    }
-
-    public static class HW2503173 {
-    }
 }
+
