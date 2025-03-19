b40fef059c48497d73e344c26b95cbbf7724ade2
isyouyoung <2520110191@gspace.kopo.ac.kr>
2025-03-19 12:55:09 +0900

    2025-03-19 수 (숙제 1번)

---
+++ src/main/java/Hw25031901.java
@@ -0,0 +1,11 @@
+// 250319 부터는 난생처음 자바 프로그래밍 교제에 나오는 코드로
+// 처음부터 다시 작성하였습니다
+
+// p66 코드 2-2
+public class Hw25031901{
+    public static void main(String[] args){
+        int a = 100;
+        int b = 200;
+    }
+}
+// 아무것도 출력되지 않음
