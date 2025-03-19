d29d0cf4d6f17de9ad82b4f5bcb7342e90fc2f1f
isyouyoung <2520110191@gspace.kopo.ac.kr>
2025-03-19 12:59:56 +0900

    2025-03-19 수 (숙제 3번)

---
+++ src/main/java/Hw25031903.java
@@ -0,0 +1,12 @@
+// p69 코드 2-5
+public class Hw25031903{
+    public static void main(String[] args){
+        int a = 100;
+        int b = 200;
+
+        int c = a + b;
+        System.out.println(a + "+" + b + "=" + c);
+    }
+}
+// 아무것도 출력되지 않음
+// 왜냐하면 a 의 100과 b 의 200의 합인 값인 300을 c 그릇에 넣기만 했기 때문임
