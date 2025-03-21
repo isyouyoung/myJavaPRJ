867fd8e39bdbc82f9ab10b3e9487301b5498b5db
isyouyoung <2520110191@gspace.kopo.ac.kr>
2025-03-21 15:09:23 +0900

    2025-03-21 금 스프링부트 프레임워크

---
+++ src/main/java/St25032104.java
@@ -0,0 +1,17 @@
+public class St25032104 {
+    public static void main(String[] args) {
+        int num1;
+        double num2;
+        String str;
+
+        str = "1234";
+        num1 = Integer.parseInt(str);
+        System.out.println("문자열 ==> 정수 : " + num1);
+
+        str = "3.14";
+        num2 = Double.parseDouble(str);
+        System.out.println("문자열 ==> 실수 : " +num2);
+
+        // 난생처음 자바 프로그래밍 p91 코드 2-12
+    }
+}
