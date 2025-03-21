9bea33f3de06072c32e53dbc5e0e63484751f968
isyouyoung <2520110191@gspace.kopo.ac.kr>
2025-03-21 13:16:39 +0900

    2025-03-21 금 스프링부트 프레임워크 01

---
+++ src/main/java/St25032101.java
@@ -0,0 +1,15 @@
+// 2025-03-21 금
+// 스프링부트 프레임워크 수업
+
+public class St25032101 {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+
+        int c = a + b;
+
+        System.out.println(c);
+
+        System.out.println(a + "+" + b + "=" + c);
+    }
+}
