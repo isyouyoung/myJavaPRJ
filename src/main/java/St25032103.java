219f12513bb1d67e6198eb28fa4063a81d56fb9f
isyouyoung <2520110191@gspace.kopo.ac.kr>
2025-03-21 14:54:52 +0900

    2025-03-21 금 스프링부트 프레임워크

---
+++ src/main/java/St25032103.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+// 가져옵니다 자바폴더안에 유틸볼더안에 스캐너를
+
+public class St25032103 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        //S로 시작해서 클래스 s 는 변수명이고
+        // new는 클래스에 다 붙임 유일하게 String만 예외!! <= 많이쓰여서
+        // System.in 은 입력값
+
+        int num;
+
+        num = s.nextInt();
+        System.out.println("사용자가 입력한 값 ==> " +num);
+
+        s.close();
+    }
+}
