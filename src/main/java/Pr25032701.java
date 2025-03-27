aeb42b2a89a4878d7729b169aee005ba69e9f36e
isyouyoung <isyouyoung@gmail.com>
2025-03-27 09:09:45 +0900

    2025-03-27 쪽지시험 공부4

---
+++ src/main/java/Pr25032701.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pr25032701 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("정수 a 를 입력하세요 : ");
+        int a = s.nextInt();
+
+        System.out.print("정수 b 를 입력하세요 : ");
+        int b = s.nextInt();
+
+        double result = (double) a / b;
+        System.out.printf("%d / %d = %.1f ", a, b, result );
+
+        s.close();
+
+
+    }
+}
