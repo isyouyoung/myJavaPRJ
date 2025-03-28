3acb2fd61015355ae74ee9a640482f5dd83389de
isyouyoung <isyouyoung@gmail.com>
2025-03-28 13:23:05 +0900

    2025-03-28 수업 코드 3-9

---
+++ src/main/java/St25032802.java
@@ -0,0 +1,12 @@
+public class St25032802 {
+    public static void main(String[] args) {
+        int num = 20;
+        num++ ; System.out.print(num+" ");
+        num-- ; System.out.print(num+" ");
+        num += 3 ; System.out.print(num+" ");
+        num -= 3 ; System.out.print(num+" ");
+        num *= 3 ; System.out.print(num+" ");
+        num /= 3 ; System.out.print(num+" ");
+        num %= 3 ; System.out.print(num+" ");
+    }
+}
