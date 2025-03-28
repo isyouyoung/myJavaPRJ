b614d2add1b7df6d2a0b9160c2ad56ad35394d36
isyouyoung <isyouyoung@gmail.com>
2025-03-28 12:25:53 +0900

    2025-03-28 쪽지시험 공부2

--- src/main/java/Exam01.java
+++ src/main/java/Exam01.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class Exam01 {
     public static void main(String[] args) {
         // Scanner 객체 생성 (입력 받기 위함)
-        Scanner scanner = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
         // 정수 a, b 입력 받기
         System.out.print("정수 a 입력: ");
-        int a = scanner.nextInt();
+        int a = s.nextInt();
 
         System.out.print("정수 b 입력: ");
-        int b = scanner.nextInt();
+        int b = s.nextInt();
 
         // 나누기 연산 (소수점 포함)
         double result = (double) a / b;
 
         // 결과 출력
-        System.out.printf("%d / %d = %.1f\n", a, b, result);
+        System.out.printf("%d / %d = %.1f", a, b, result);
 
         // Scanner 닫기
-        scanner.close();
+        s.close();
     }
 }
