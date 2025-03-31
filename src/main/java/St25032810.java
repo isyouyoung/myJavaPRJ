2a924c7434eb4c14b761f1c6b9e31ea8d8acfa1d
isyouyoung <isyouyoung@gmail.com>
2025-03-31 18:46:16 +0900

    2025-03-31 복습 04 2025-03-28 수업 코드 3-9

---
+++ src/main/java/St25032810.java
@@ -0,0 +1,15 @@
+public class St25032810 {
+    public static void main(String[] args){
+        String ss = "First Java를 공부 중입니다";
+        String var1, var2;
+
+        var1 = ss.toUpperCase();
+        // ss를 대문자로 바꾸고 var1에 저장
+        System.out.println(var1);
+        // var1 출력
+        var2 = ss.toLowerCase();
+        // ss를 소문자로 바꾸고 var2에 저장
+        System.out.println(var2);
+        // var2 출력
+    }
+}
