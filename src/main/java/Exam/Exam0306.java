d4bdd2f5e1822a0354c95034b74f83621fa59c1a
isyouyoung <isyouyoung@gmail.com>
2025-04-03 16:01:05 +0900

    쪽지시험 문제

---
+++ src/main/java/Exam/Exam0306.java
@@ -0,0 +1,17 @@
+package Exam;
+
+import java.util.Scanner;
+
+public class Exam0306 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String a = s.next();
+        String b = s.next();
+
+        String result = (a+b).toLowerCase().replaceAll(" ","");
+        System.out.print(result);
+
+        s.close();
+    }
+}
