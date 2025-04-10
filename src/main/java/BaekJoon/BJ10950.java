53c51b42299425b4812a81a53cfe839e10c006f5
isyouyoung <isyouyoung@gmail.com>
2025-04-10 16:10:46 +0900

    백준문제 및 수업

---
+++ src/main/java/BaekJoon/BJ10950.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BJ10950 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int T = s.nextInt();
+
+
+        for (int i = 0; i < T; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            int result = (a+b);
+            System.out.println(result);
+        }
+
+    }
+}
+// 2025-04-08 2학년 선배님들 특강 문제1
---
+++ src/main/java/StudyClass/St25041002.java
@@ -0,0 +1,14 @@
+package StudyClass;
+
+public class St25041002 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i < 9; i++) { // 2단부터 9단까지
+
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i + "x" + j + "=" + i * j + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
