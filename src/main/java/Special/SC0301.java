0e6ed0d543d655dfeeee4c6e05cdee75394869f4
isyouyoung <isyouyoung@gmail.com>
2025-04-15 20:11:18 +0900

    공부

---
+++ src/main/java/Special/SC0301.java
@@ -0,0 +1,16 @@
+package Special;
+
+class Rabbit {
+    String shape;
+    int xPos;
+    int yPos;
+
+            Rabbit() {
+                shape = "토끼";
+            }
+
+            void setPosition(int x, int y) {
+                xPos = x;
+                yPos = y;
+            }
+}
