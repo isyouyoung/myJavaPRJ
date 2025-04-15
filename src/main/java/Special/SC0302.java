f2a3bcb182e3a490b4d8c2ba2f71b7a3f7df137e
isyouyoung <isyouyoung@gmail.com>
2025-04-15 20:11:43 +0900

    공부

---
+++ src/main/java/Special/SC0302.java
@@ -0,0 +1,16 @@
+package Special;
+
+class Rabbit2 {
+    public  String shape;
+    public  int xPos;
+    public  int yPos;
+}
+
+public class SC0302 {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+
+        rabbit.xPos = 100;
+        rabbit.yPos = 200;
+    }
+}
