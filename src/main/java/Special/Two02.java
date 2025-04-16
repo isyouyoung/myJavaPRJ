8a921cc5d81140c9e13d1cb3fd3edc0ce3db8360
isyouyoung <isyouyoung@gmail.com>
2025-04-17 08:44:22 +0900

    공부

---
+++ src/main/java/Special/Two02.java
@@ -0,0 +1,17 @@
+package Special;
+
+public class Two02 {
+    public static void main(String[] args) {
+
+        int result = 0;
+
+        for(int i = 1; i <= 100; i++){
+            result += i;
+        }
+
+        System.out.println(result);
+        
+    }
+}
+
+// 1부터 100까지의 합을 더하는 예제
