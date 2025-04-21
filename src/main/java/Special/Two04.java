8d116430713824f3644499c9aaee2e948196db4c
isyouyoung <isyouyoung@gmail.com>
2025-04-21 19:15:41 +0900

    공부

---
+++ src/main/java/Special/Two04.java
@@ -0,0 +1,14 @@
+package Special;
+
+public class Two04 {
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j<= 2; j++){
+                System.out.println(i+"번째 외부 for문에서 "+j+"번째 내부 for문");
+            }
+        }
+    }
+
+
+}
