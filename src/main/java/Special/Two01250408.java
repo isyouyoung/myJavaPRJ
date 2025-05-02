586b59021bd574b593a8bfe620ee7aaee7d10059
isyouyoung <isyouyoung@gmail.com>
2025-05-02 20:59:51 +0900

    공부

---
+++ src/main/java/Special/Two01250408.java
@@ -0,0 +1,16 @@
+package Special;
+
+public class Two01250408 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <=5; i++){
+            System.out.println(i + "번째 반복");
+        }
+
+        System.out.println();
+
+        for(int i = 5; i >= 1; i--){
+            System.out.println(i + "번 말할거야");
+        }
+    }
+}
