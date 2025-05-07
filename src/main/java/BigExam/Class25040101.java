0a62e44253a6d1d076cc425b689c77ecd38d5a42
isyouyoung <isyouyoung@gmail.com>
2025-05-07 21:56:59 +0900

    공부

---
+++ src/main/java/BigExam/Class25040101.java
@@ -0,0 +1,17 @@
+package BigExam;
+
+public class Class25040101 {
+    public static void main(String[] args) {
+
+        String st = "*";
+        String bl = " ";
+
+        int a = 4;
+
+        for (int i = 1; i <= a; i++) {
+            System.out.print(bl.repeat(a - i));
+            System.out.print(st.repeat(2 * i - 1));
+            System.out.println();
+        }
+    }
+}
