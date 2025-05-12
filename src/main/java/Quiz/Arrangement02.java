12ff2715535690ea10f4933f609405f740881a5d
isyouyoung <isyouyoung@gmail.com>
2025-05-12 20:16:01 +0900

    공부

--- src/main/java/Quiz/Arrangement02.java
+++ src/main/java/Quiz/Arrangement02.java
@@ -1,6 +1,6 @@
 package Quiz;
 
-public class Arrangement0201 {
+public class Arrangement02 {
     public static void main(String[] args) {
 
         int[] nums = {1, 3, 7, 1, 4, 7, 3, 1, 9, 0, 7, 3};
