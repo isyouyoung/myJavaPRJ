4d0254dd3acf9c198e129ddecd2702108312eb56
isyouyoung <isyouyoung@gmail.com>
2025-05-17 17:25:28 +0900

    공부

---
+++ src/main/java/BigExam/Class25040204.java
@@ -0,0 +1,15 @@
+package BigExam;
+
+public class Class25040204 {
+    public static void main(String[] args) {
+
+        String log = "127.0.01 - frank [10/Oct/2000:13:55:36 -0700] \"GET /apache_pb.gif HTTP/1.0\" 200";
+
+        int start = log.indexOf("GET") + 4;
+        int end = log.indexOf("HTTP") - 1;
+
+        String url = log.substring(start,end);
+
+        System.out.println(url);
+    }
+}
