5fa07336ce9a30c19ca79778b6610c0a608e88b4
isyouyoung <isyouyoung@gmail.com>
2025-05-27 20:57:17 +0900

    공부

---
+++ src/main/java/Exam/Exam0505.java
@@ -0,0 +1,10 @@
+package Exam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exam0505 {
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+    }
+}
