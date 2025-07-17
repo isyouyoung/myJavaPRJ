bace74e423e864a859c34bb60559443e86b0a661
isyouyoung <isyouyoung@gmail.com>
2025-07-17 16:36:58 +0900

    BJ1000

---
+++ src/main/java/BaekJoon/BJ01000.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BJ01000 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + num2);
+    }
+}
